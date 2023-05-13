0a2b9f5e71d13cda6d9a3cac95b0cbdd6d578ff4
qi xu <qi.xu@carrier.com>
2023-05-13 10:45:58 +0200

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+Nodemailer is a module for Node.js applications to allow easy as cake email sending. The project got started back in 2010 when there was no sane option to send email messages, today it is the solution most Node.js users turn to by default.
+
+link https://nodemailer.com/about/
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendmail() {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'qi.xu@example.com', // generated ethereal user
+      pass: '*******', // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Qi Xu" <bobo_xu@hotmail.com>', // sender address
+    to: 'qi.xu@example.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello Qi, From nodemailer nodejs app', // plain text body
+    html: '<b>Hello Qi</b>', // html body
+    attachments: [
+      {
+        // file on disk as an attachment
+        filename: 'image.jpg',
+        path: 'images/img.png', // replace with the path to the file
+      },
+      // you can add more attachments if needed
+    ],
+  });
+
+  console.log('Message sent: %s', info.messageId);
+}
+
+sendmail().catch(console.error);
